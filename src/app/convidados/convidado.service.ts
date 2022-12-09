ce0d00efb332c9bcfb37b71451979f7cd552c4b3
Marcossakaguchi5 <jorge.sakaguchi@outlook.com>
2022-12-09 01:15:55 -0300

    color button

--- src/app/convidados/convidado.service.ts
+++ src/app/convidados/convidado.service.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { environment } from './../../environments/environment.prod';
 import { Injectable } from '@angular/core';
 
@@ -8,5 +10,19 @@ const API=environment.apiURL;
 export class ConvidadoService {
 private readonly API= `${API}/`
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  edit(id_evento:any,evento:any):Observable<any>{
+    return this.http.put<any>(`${API}/evento/${id_evento}`,evento)
+
+  }
+ buscar:any=(id_evento:any)=>{
+    return this.buscar(`${API}/${id_evento}`)
+
+  }
+  delet(id_evento:any){
+    return this.http.delete(`${API}/evento/${id_evento}`)
+  }
 }
--- src/app/convidados/lista-convidados/lista-convidados.component.ts
+++ src/app/convidados/lista-convidados/lista-convidados.component.ts
@@ -1,16 +1,56 @@
+import { EditarEventoComponent } from './../../eventos/editar-evento/editar-evento.component';
+import { ConvidadoService } from './../convidado.service';
 import { ModalController } from '@ionic/angular';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista-convidados',
   templateUrl: './lista-convidados.component.html',
   styleUrls: ['./lista-convidados.component.scss']
 })
-export class ListaConvidadosComponent implements OnInit {
 
-  constructor(private modalcontroller:ModalController) { }
+export class ListaConvidadosComponent implements OnInit {
+private  id_evento:any;
+evento={
+  descricao:'',
+  data_hora:'',
+}
+  constructor(
+    private modalcontroller:ModalController,
+    private route:ActivatedRoute,
+    private service:ConvidadoService,
+    private router:Router
+    ) { this.route.params.subscribe(params=> this.id_evento=params['id_evento'])
+ // this.service.buscar(this.id_evento).subscribe((evento:any)=>{
+    //this.evento=evento
+//  })
+}
 
   ngOnInit(): void {
+  }
+  // async edit(){
+  //   const modal= await this.modalcontroller.create({
+  //     component:EditarEventoComponent,
+      
+  //     componentProps:{this.id_evento,this.evento,
+       
+  //     },
+  //     cssClass:"modal",
+  //   });
+  //   await modal.present();
+     
+    
+  // }
+
+  delet(){
+    console.log(this.id_evento)
+    this.service.delet(this.id_evento).subscribe(
+      ()=>{
+        this.router.navigate(["/eventos"])
+
+      }
+    )
   }
 
 }
